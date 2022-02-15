7ac8d904b13cc501b3e052aef1a7e57640fd5fb9
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-02-15 20:09:09 -0300

    Sub-Topic: Measures of Central Tendency - Part3 -> add | Next Sub-Sub-Topic: Weighted Mean

--- autogit.sh
+++ autogit.sh
@@ -1,4 +1,4 @@
 git add -A
-git commit -m "Sub-Topic: Measures of Central Tendency - Part3 -> add | Next Sub-Topic: Weighted Mean"
+git commit -m "Sub-Topic: Measures of Central Tendency - Part3 -> add | Next Sub-Sub-Topic: Weighted Mean"
 git push
 git status
